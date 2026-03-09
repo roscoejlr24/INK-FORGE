e39681ca4bc5d966a50cde060382ec39d24f1afe
roscoejlr24 <147072819+roscoejlr24@users.noreply.github.com>
2026-03-09 07:24:28 -0500

    Add TattooCreator component

---
+++ src/components/TattooCreator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TattooCreator = () => {
+    return (
+        <div>
+            <h1>Tattoo Creator</h1>
+            <p>Create your own tattoo design here!</p>
+        </div>
+    );
+};
+
+export default TattooCreator;
